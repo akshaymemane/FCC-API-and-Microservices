07b206c6807f99d04b7697dd1c8feeb4505363a3
DESKTOP-IRGEPDR\Coditas <akshay.memane.official@gmail.com>
2021-05-30 00:50:58 +0530

    Use the .env File

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+require('dotenv').config();
 
 //1
 console.log("Hello World");
@@ -18,9 +19,19 @@ app.get("/",(req,res)=>{
 app.use('/public', express.static(__dirname + '/public'));
 
 //5
+// app.get("/json",(req,res)=>{
+//     res.json({"message": "Hello json"});
+// });
+
+//6
 app.get("/json",(req,res)=>{
-    res.json({"message": "Hello json"});
+    if(process.env.MESSAGE_STYLE === 'uppercase'){
+        res.json({"message": "HELLO JSON"});
+    }else{
+        res.json({"message": "Hello json"});
+    }
 });
+
 
 
 
