f4acf10e69c2627c8fad97f58a03fddf54173ae4
DESKTOP-IRGEPDR\Coditas <akshay.memane.official@gmail.com>
2021-05-30 00:34:36 +0530

    sendFile added

--- myApp.js
+++ myApp.js
@@ -1,14 +1,18 @@
 var express = require('express');
 var app = express();
 
+//1
 console.log("Hello World");
 
+//2
+// app.get("/",(req,res)=>{
+//     res.send("Hello Express");
+//   });
 
-
-
+//3
 
 app.get("/",(req,res)=>{
-    res.send("Hello Express");
+    res.sendFile(__dirname + "/views/index.html");
   });
 
 
